310cddd64a66b97a5f0237909cb87035f13a1320
ykomuro0719 <y_komuro@r.recruit.co.jp>
2017-10-17 17:26:32 +0900

    initial

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,30 @@
+class TasksController < ApplicationController
+  def index
+  @tasks = Task.all
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.create(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+  def show
+    @task = Task.find(params[:id])
+  end
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+  end
+  def destroy
+  end
+
+private
+  def task_params
+    params.require(:task).permit(:title)
+  end
+end
